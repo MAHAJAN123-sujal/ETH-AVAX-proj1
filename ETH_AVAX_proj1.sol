f6684bfa57498d18a46b8ef5c4cbebd7e708505f
MAHAJAN123-sujal <158045134+MAHAJAN123-sujal@users.noreply.github.com>
2024-06-12 23:28:47 +0530

    Update ETH_AVAX_proj1.sol

--- ETH_AVAX_proj1.sol
+++ ETH_AVAX_proj1.sol
@@ -2,41 +2,49 @@
 pragma solidity ^0.8.7;
 
 contract ETH_AVAX_proj1{
-    address public owner;
-    uint tokens=100;
-    mapping (address => uint) public addressToBalance;
+    address public teacher;
+    mapping (address =>string) public students;
+    mapping (address => bool) public studentExists;
+    mapping (address => bool) public attendance;
+    mapping (address => uint) public studMarks;
+
+    event StudentAdded(address indexed student, string name);
+    event AttendanceRecorded(address indexed student, bool present);
+    event MarksAssigned(address indexed student, uint marks);
 
     constructor(){
-        owner = msg.sender;
-        addressToBalance[msg.sender] = tokens;
+        teacher = msg.sender;
     }
 
-    function deposit(uint256 _amount) public {
-        uint256 previousBalance = addressToBalance[msg.sender];
-        addressToBalance[msg.sender] += _amount;
-
-    // Here I used assert function to check if the deposited amount is reflected to his account
-        assert(addressToBalance[msg.sender] == previousBalance + _amount);
+    function addStudent(address _student,string memory _name)public{
+        require(teacher==msg.sender,"Only teacher can perform these tasks");
+        students[_student] = _name;
+        attendance[_student] = false; // Initialize attendance as absent
+        studMarks[_student] = 0; // Initialize marks
+        studentExists[_student] = true;
+        emit StudentAdded(_student, _name);
     }
 
-    function transfer(address _to,uint _amount) public{
-    // Use of require to check for owner and present balance
-        require(owner == msg.sender,"Can't make transaction");
-        require(addressToBalance[owner]>=_amount,"Insufficient balance");
-        addressToBalance[owner] -= _amount;
-        addressToBalance[_to] += _amount;
+    function recordAttendance(address _student, bool att) public{
+        require(studentExists[_student] == true, "Student not registered");
+        
+        attendance[_student] = att;
+
+        // Assert to make sure attendance is correctly recorded
+        assert(attendance[_student] == att);
+        emit AttendanceRecorded(_student, att);
     }
 
-    function withdraw(uint _amount) public{
-    //  Here, I used assert function to make sure that only owner is able to withdraw money
-        assert(owner == msg.sender);
-        if(addressToBalance[owner]>=_amount){
-            addressToBalance[owner] -= _amount;
+    function assignMarks(address _student, uint _marks) public{
+        require(studentExists[_student] == true, "Student not registered");
+        if(_marks>=0 && _marks<=100){
+            studMarks[_student] = _marks;
+            // Assert to make sure marks are correctly assigned
+            assert(studMarks[_student] == _marks);
+            emit MarksAssigned(_student, _marks);
         }
         else{
-        // Here, I used revert function to revert back in case, he is asking for more amount
-        // than he has in his account
-            revert("Insufficient Balance to withdraw money");
+            revert("Marks are not in correct range");
         }
     }
 }
